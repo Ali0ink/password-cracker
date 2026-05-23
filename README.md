c945bd9cce45d73b3f44c7136c69d47e8558735c
Ali0ink <alih033einy@gmail.com>
2026-05-23 20:03:51 +0300

    improve result handling and add performance output for both case

---
+++ README.md
@@ -0,0 +1,5 @@
+## Wordlist
+
+for running cracker you need a wordlist like rockyou.txt
+
+cause of the size and security file is not in the repo
--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import hashlib
+import time
+
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
-
-def load_file():
-    
-        return file.readlines()
         
 
 def password_cracker(target_hash):
+    
+    count = 0
     with open("rockyou_2025_00.txt", "r", encoding="utf-8", errors="ignore")as file:
 
         for line in file:
             password = line.strip()
-
+            count += 1
             if hash_password(password) == target_hash:
-                print(f"["+("="*50)+"]")
-                print(f"password found: {password}")
-                print(f"["+("="*50)+"]")
-                return password
-        print("password not found")
+                return True, password , count
+        
+        return False, None , count
+def main():
+    start = time.time()
 
-password_cracker(hash_password("1234"))
+    result = password_cracker(hash_password("mmmmmvvvvvvvvvvvvvvvv"))
+
+    condition, password , count = result
+    end = time.time()
+
+    duration = end -start
+    speed = count / duration
+
+
+    if condition:
+        
+        print("="*50)
+        print(f"password found : {password}")
+        print(f"Tried {count} passwords in {duration:.2f} seconds")
+        print(f"speed: {speed:.2f} passwords/sec")
+        print("="*50)
+    else:
+        
+        print("="*50)
+        print("password not found")
+        print(f"Tried {count} passwords in {duration:.2f} seconds")
+        print(f"speed: {speed:.2f} passwords/sec")
+        print("="*50)
+
+main()
